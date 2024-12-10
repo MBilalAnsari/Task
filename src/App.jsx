c1a37738cb10ae12dcea77fa60b9db6ac190fd4a
Bilal Ansari <m.bilal.ansari.qadri.786@gmail.com>
2024-12-10 22:22:55 +0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-// import Home from "./pages/Home"
+import Home from "./pages/Home"
 
 
 
@@ -10,7 +10,7 @@ export default function App() {
   return (
     <>
       <div className='border-[3px]  bg-[#0F141E] text-white'>
-        {/* <Home /> */}
+        <Home />
       </div>
     </>
   )
